5be7646b196a25f4f915c8b7c1bcca3cc3a96aa8
becoze <liyuanliang20@outlook.com>
2024-10-31 23:16:15 +1100

    project init

---
+++ src/main/java/com/becoze/nickoj/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.becoze.nickoj.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.becoze.nickoj.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
