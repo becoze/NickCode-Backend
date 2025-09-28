1cbaa3cf1abc158ef8f1e452491bfffc5d774e3e
becoze <liyuanliang20@outlook.com>
2025-09-28 17:52:41 +1000

    feat: sql mybatis mapping

---
+++ src/main/java/com/becoze/nickcode/mapper/ProblemMapper.java
@@ -0,0 +1,18 @@
+package com.becoze.nickcode.mapper;
+
+import com.becoze.nickcode.model.entity.Problem;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author liyua
+* @description 针对表【problem(Problem)】的数据库操作Mapper
+* @createDate 2025-08-20 19:47:05
+* @Entity com.becoze.nickcode.model.entity.Problem
+*/
+public interface ProblemMapper extends BaseMapper<Problem> {
+
+}
+
+
+
+
---
+++ src/main/java/com/becoze/nickcode/mapper/ProblemSubmitMapper.java
@@ -0,0 +1,18 @@
+package com.becoze.nickcode.mapper;
+
+import com.becoze.nickcode.model.entity.ProblemSubmit;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author liyua
+* @description 针对表【problem_submit(Problem_Submit)】的数据库操作Mapper
+* @createDate 2025-08-20 19:49:31
+* @Entity com.becoze.nickcode.model.entity.ProblemSubmit
+*/
+public interface ProblemSubmitMapper extends BaseMapper<ProblemSubmit> {
+
+}
+
+
+
+
