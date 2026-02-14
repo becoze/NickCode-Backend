96078a65371db112cccc4ee8eaf79ca9e4b7105e
becoze <liyuanliang20@outlook.com>
2026-02-14 16:28:13 +1100

    fix: change user code language submition enum to match monaco

--- src/main/java/com/becoze/nickcode/model/enums/ProblemSubmitLanguageEnum.java
+++ src/main/java/com/becoze/nickcode/model/enums/ProblemSubmitLanguageEnum.java
@@ -15,8 +15,8 @@ import java.util.stream.Collectors;
 public enum ProblemSubmitLanguageEnum {
 
     JAVA("java", "java"),
-    CPLUSPLUS("c++", "c++"),
-    GOLANG("goland", "goland");
+    CPLUSPLUS("cpp", "cpp"),
+    GOLANG("go", "go");
 
     private final String text;
 
