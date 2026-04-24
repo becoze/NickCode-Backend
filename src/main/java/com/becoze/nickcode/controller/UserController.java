3695d066e3d2cf71bcbf70c55616be2344386706
becoze <liyuanliang20@outlook.com>
2026-04-24 17:23:22 +1000

    fix: display logged-in username

--- src/main/java/com/becoze/nickcode/controller/UserController.java
+++ src/main/java/com/becoze/nickcode/controller/UserController.java
@@ -73,10 +73,11 @@ public class UserController {
         String userAccount = userRegisterRequest.getUserAccount();
         String userPassword = userRegisterRequest.getUserPassword();
         String checkPassword = userRegisterRequest.getCheckPassword();
+        String userName = userRegisterRequest.getUserAccount();
         if (StringUtils.isAnyBlank(userAccount, userPassword, checkPassword)) {
             return null;
         }
-        long result = userService.userRegister(userAccount, userPassword, checkPassword);
+        long result = userService.userRegister(userAccount, userPassword, checkPassword, userName);
         return ResultUtils.success(result);
     }
 
