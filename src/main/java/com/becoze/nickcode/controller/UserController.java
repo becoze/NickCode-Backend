b42b8e80bbc69065a07dec693ac0f261f578e998
becoze <liyuanliang20@outlook.com>
2026-06-19 22:11:21 +1000

    chore: Remove all Wx (We Chat) related content

--- src/main/java/com/becoze/nickcode/controller/UserController.java
+++ src/main/java/com/becoze/nickcode/controller/UserController.java
@@ -6,7 +6,6 @@ import com.becoze.nickcode.common.BaseResponse;
 import com.becoze.nickcode.common.DeleteRequest;
 import com.becoze.nickcode.common.ErrorCode;
 import com.becoze.nickcode.common.ResultUtils;
-import com.becoze.nickcode.config.WxOpenConfig;
 import com.becoze.nickcode.constant.UserConstant;
 import com.becoze.nickcode.exception.BusinessException;
 import com.becoze.nickcode.exception.ThrowUtils;
@@ -24,12 +23,8 @@ import com.becoze.nickcode.service.UserService;
 import java.util.List;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import lombok.extern.slf4j.Slf4j;
-import me.chanjar.weixin.common.bean.WxOAuth2UserInfo;
-import me.chanjar.weixin.common.bean.oauth2.WxOAuth2AccessToken;
-import me.chanjar.weixin.mp.api.WxMpService;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.util.DigestUtils;
@@ -37,7 +32,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import static com.becoze.nickcode.service.impl.UserServiceImpl.SALT;
@@ -53,9 +47,6 @@ public class UserController {
 
     @Resource
     private UserService userService;
-
-    @Resource
-    private WxOpenConfig wxOpenConfig;
 
     // region 登录相关
 
@@ -100,29 +91,6 @@ public class UserController {
         }
         LoginUserVO loginUserVO = userService.userLogin(userAccount, userPassword, request);
         return ResultUtils.success(loginUserVO);
-    }
-
-    /**
-     * 用户登录（微信开放平台）
-     */
-    @GetMapping("/login/wx_open")
-    public BaseResponse<LoginUserVO> userLoginByWxOpen(HttpServletRequest request, HttpServletResponse response,
-            @RequestParam("code") String code) {
-        WxOAuth2AccessToken accessToken;
-        try {
-            WxMpService wxService = wxOpenConfig.getWxMpService();
-            accessToken = wxService.getOAuth2Service().getAccessToken(code);
-            WxOAuth2UserInfo userInfo = wxService.getOAuth2Service().getUserInfo(accessToken, code);
-            String unionId = userInfo.getUnionId();
-            String mpOpenId = userInfo.getOpenid();
-            if (StringUtils.isAnyBlank(unionId, mpOpenId)) {
-                throw new BusinessException(ErrorCode.SYSTEM_ERROR, "Login fail, System Error");
-            }
-            return ResultUtils.success(userService.userLoginByMpOpen(userInfo, request));
-        } catch (Exception e) {
-            log.error("userLoginByWxOpen error", e);
-            throw new BusinessException(ErrorCode.SYSTEM_ERROR, "Login fail, System Error");
-        }
     }
 
     /**
